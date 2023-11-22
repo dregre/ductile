2db2a52aa2241b30601b6e8e0973ab12c5bac89f
Andre Gregori <andre@gregori.org>
2023-11-22 12:47:58 -0500

    Bracket syntax.

--- pipe.py
+++ pipe.py
@@ -1,70 +1,39 @@
-import types
 from typing import Any
-
 from itertools import chain
 
 class Pipe:
-    def __init__(self, val) -> None:
+    def __init__(self, val):
         self.val = val
-
-    def __call__(self, *args: Any, **kwds: Any) -> Any:
+    
+    def __getitem__(self, arg):
+        if isinstance(arg, tuple):
+            (fn, order) = arg
+        else:
+            (fn, order) = (arg, 1)
+        return PipeCallable(fn, self.eval(), order)
+    
+    def eval(self):
         return self.val
 
-    def __truediv__(self, nextfn):
-        if isinstance(nextfn, PipeCallable):
-            nextfn.args = chain((self(),), nextfn.args)
-            return nextfn
-        return PipeCallable(nextfn, (self(),))
-    
-    def __floordiv__(self, nextfn):
-        if isinstance(nextfn, PipeCallable):
-            nextfn.args = chain(nextfn.args, (self(),))
-            return nextfn
-        result = PipeCallable(nextfn)
-        result.lastargs = (self(),)
-        return result
-    
     def __invert__(self):
-        return self()
+        return self.val
 
-class PipeCallable:
-    def __init__(self, fn, args=tuple(), kwargs=dict(), lastargs=tuple()) -> None:
+class PipeCallable(Pipe):
+    def eval(self):
+        args = chain([self.val], self.args) if self.order == 1 else chain(self.args, [self.val])
+        return self.fn(*args, **self.kwargs)
+
+    def __init__(self, fn, val, order) -> None:
         self.fn = fn
-        self.args = args
-        self.kwargs = kwargs
-        self.lastargs = lastargs
-
-    def __call__(self, *args: Any, **kwds: Any) -> Any:
-        val = self.fn(*chain(self.args, args, self.lastargs), **(self.kwargs | kwds))
+        self.val = val
+        self.order = order
         self.args = tuple()
         self.kwargs = dict()
-        return val
-    
-    def __mul__(self, other):
-        self.args = chain(self.args, other)
-        return self
-    
-    def __mod__(self, other):
-        self.kwargs = self.kwargs | other
-        return self
-
-    def __truediv__(self, nextfn):
-        if isinstance(nextfn, PipeCallable):
-            nextfn.args = chain((self(),), nextfn.args)
-            return nextfn
-        return PipeCallable(nextfn, (self(),))
-    
-    def __floordiv__(self, nextfn):
-        if isinstance(nextfn, PipeCallable):
-            nextfn.args = chain(nextfn.args, (self(),))
-            return nextfn
-        result = PipeCallable(nextfn)
-        result.lastargs = (self(),)
-        return result
     
     def __invert__(self):
-        return self()
-        
+        return self.eval()
 
-def pipe(value):
-    pass
+    def __call__(self, *args: Any, **kwargs: Any) -> Any:
+        self.args = args
+        self.kwargs = kwargs
+        return self
